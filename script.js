d71dc83f9cf59f20da99b735a7806a195eb616aa
Alastair Coote <alastair.coote@nytimes.com>
2024-06-07 23:34:18 -0400

    more fixes

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
-const randomColors = [
-  "hsl(357,27%,93%)",
-  "hsl(7,41%,73%)",
-  "hsl(121,20%,62%)",
-  "hsl(236,89%,5%)",
-  "hsl(29,28%,38%)",
-  "hsl(210,34%,39%)",
-  "hsl(107,49%,59%)",
-  "hsl(17,56%,49%)",
-  "hsl(153,31%,48%)",
-  "hsl(161,24%,5%)",
-];
-
-document.body.style.color = randomColors[0];
+const h1 = document.querySelector("h1");
+const select = document.querySelector("select");
+const freeze = document.querySelector("#freeze");
+const dateFormatter = new Intl.DateTimeFormat("en-US", { timeStyle: "medium" });
 
 if (window.top === window) {
   const iframe = document.createElement("iframe");
+  document.querySelector("#iframe-target").appendChild(iframe);
 
-  const url = new URL("http://127.0.0.1/index.html");
-  url.port = window.location.port;
+  select.addEventListener("change", function (e) {
+    const targetURL = new URL(location.href);
+    if (select.value === "cross-origin") {
+      if (targetURL.hostname === "localhost") {
+        targetURL.hostname = "127.0.0.1";
+      } else {
+        const hostSplit = targetURL.hostname.split(".");
+        hostSplit[0] = "second-origin";
 
-  iframe.src = url.href;
-  document.body.appendChild(iframe);
-  window.addEventListener("message", function () {
-    //   console.log("msg", iframe.contentDocument);
-    start();
+        targetURL.hostname = hostSplit.join(".");
+      }
+    }
+
+    iframe.src = targetURL.href;
   });
-} else {
-  window.top.postMessage("start", "*");
-  start();
+
+  select.dispatchEvent(new CustomEvent("change"));
 }
 
-const h1 = document.querySelector("h1");
-
-function start() {
-  setInterval(function () {
-    const currentVal = parseInt(h1.innerText, 10);
-    let newVal = currentVal + 1;
-    if (newVal === 10) {
-      newVal = 0;
-    }
-    h1.innerText = newVal;
-    document.body.style.color = randomColors[newVal];
-
-    if (newVal === 6 && window.top !== window) {
-      //   while (true) {
-      //     // deliberately freeze
-      //   }
-    }
+freeze.addEventListener("click", function () {
+  // deliberately freeze
+  setTimeout(function () {
+    while (true) {}
   }, 1000);
+});
+
+function setTime() {
+  h1.innerText = dateFormatter.format(Date.now());
 }
+
+const rightNow = Date.now();
+
+const untilNextSecond = rightNow % 1000;
+setTimeout(function () {
+  setTime();
+  setInterval(setTime, 1000);
+}, 1000 - untilNextSecond);
+
+// if (window.top !== window) {
+//   setTimeout(function () {
+//     while (true) {}
+//   }, 5000);
+// }
+
+setTime();
